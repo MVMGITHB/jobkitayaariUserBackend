480dc6730e3494795aca8f00e0584935c7e324af
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-09-01 17:11:14 +0530

    cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
